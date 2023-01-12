747c3b929c80d09082ce5baea50f2d9311e78279
Dennis Zoma <dennis@scio.xyz>
2023-01-12 09:00:49 +0100

    feat: Add a `useRegisteredContract` shorthand for contracts from the registry

---
+++ src/hooks/useRegisteredContract.ts
@@ -0,0 +1,18 @@
+import { useInkathon } from '@provider'
+import { getDeployment } from '@registry'
+import { useContract } from './useContract'
+
+/**
+ * React Hook that returns a `ContractPromise` object configured with
+ * the active api & chain as well as the given deployment contract id
+ * which is looked up from the deployments registry.
+ */
+export const useRegisteredContract = (
+  contractId: string,
+  networkId?: string,
+) => {
+  const { deployments, activeChain } = useInkathon()
+  networkId = networkId || activeChain?.network || ''
+  const deployment = getDeployment(deployments || [], contractId, networkId)
+  return useContract(deployment?.abi, deployment?.address)
+}
