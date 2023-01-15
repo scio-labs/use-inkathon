c3a7b8ea005aa8533b4ac3bd2a65b28a6760dd99
Dennis Zoma <dennis@scio.xyz>
2023-01-15 21:56:24 +0100

    feat: Add helper to compare arrays of accounts

--- src/helpers/accountsAreEqual.ts
+++ src/helpers/accountsAreEqual.ts
@@ -4,10 +4,20 @@ import { InjectedAccountWithMeta } from '@polkadot/extension-inject/types'
  * Returns true if both given injected accounts have the same address.
  */
 export const accountsAreEqual = (
-  acc1: InjectedAccountWithMeta,
-  acc2: InjectedAccountWithMeta,
+  a1?: InjectedAccountWithMeta,
+  a2?: InjectedAccountWithMeta,
 ) => {
-  return (
-    (acc1?.address || '').toLowerCase() === (acc2?.address || '').toLowerCase()
-  )
+  return (a1?.address || '').toLowerCase() === (a2?.address || '').toLowerCase()
+}
+
+/**
+ * Returns true if both given arrays of injected accounts contain the
+ * same objects with the same addresses in the same order.
+ */
+export const accountArraysAreEqual = (
+  a1: InjectedAccountWithMeta[],
+  a2: InjectedAccountWithMeta[],
+) => {
+  if (a1.length !== a2.length) return false
+  return a1.every((a, i) => accountsAreEqual(a, a2[i]))
 }
