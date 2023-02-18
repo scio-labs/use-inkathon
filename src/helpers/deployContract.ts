bfa51ae2b70ba90e082858cc2d55b40dc828d2fe
Dennis Zoma <dennis@scio.xyz>
2023-02-18 12:09:53 +0100

    feat: Add `deployContract` function for contract upload & instantiation

---
+++ src/helpers/deployContract.ts
@@ -0,0 +1,42 @@
+import { ApiPromise } from '@polkadot/api'
+import { CodePromise } from '@polkadot/api-contract'
+import { ContractOptions } from '@polkadot/api-contract/types'
+import { IKeyringPair } from '@polkadot/types/types'
+import { stringCamelCase } from '@polkadot/util'
+import { getMaxGasLimit } from './getGasLimit'
+
+/**
+ * Uploads & instantiates a contract on-chain.
+ */
+export const deployContract = async (
+  api: ApiPromise,
+  account: IKeyringPair | string,
+  abi: any,
+  wasm: Uint8Array | string | Buffer,
+  constructorMethod = 'new',
+  args = [] as unknown[],
+  options = {} as ContractOptions,
+) => {
+  // eslint-disable-next-line no-async-promise-executor
+  return new Promise<{ address: string; hash: string }>(async (resolve) => {
+    const code = new CodePromise(api, abi, wasm)
+    const gasLimit = getMaxGasLimit(api)
+    const constructorFn = code.tx[stringCamelCase(constructorMethod)]
+    const unsub = await constructorFn(
+      { gasLimit, ...options },
+      ...args,
+    ).signAndSend(account, ({ contract, status }: any) => {
+      if (status?.isInBlock) {
+        const hash = abi?.source.hash
+        const address = contract.address.toString()
+        console.log(`Contract '${abi?.contract.name}' deployed at ${address}`)
+
+        unsub?.()
+        resolve({
+          address,
+          hash,
+        })
+      }
+    })
+  })
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,6 +1,7 @@
 // @index('./*.(tsx|ts)', f => `export * from '${f.path}'`)
 export * from './accountsAreEqual'
 export * from './contractCall'
+export * from './deployContract'
 export * from './getGasLimit'
 export * from './unwrapResult'
 // @endindex
