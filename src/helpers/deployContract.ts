e56f078dffa9232717f947e03350e8cfca8d0f6f
Dennis Zoma <dennis@scio.xyz>
2023-02-19 16:57:06 +0100

    feat: Add error handling in `deployContract`

--- src/helpers/deployContract.ts
+++ src/helpers/deployContract.ts
@@ -1,6 +1,7 @@
 import { ApiPromise } from '@polkadot/api'
 import { CodePromise } from '@polkadot/api-contract'
 import { ContractOptions } from '@polkadot/api-contract/types'
+import { EventRecord } from '@polkadot/types/interfaces'
 import { IKeyringPair } from '@polkadot/types/types'
 import { BN, stringCamelCase } from '@polkadot/util'
 import { getGasLimit } from './getGasLimit'
@@ -17,30 +18,43 @@ export const deployContract = async (
   args = [] as unknown[],
   options = {} as ContractOptions,
 ) => {
-  // eslint-disable-next-line no-async-promise-executor
-  return new Promise<{ address: string; hash: string }>(async (resolve) => {
-    const code = new CodePromise(api, abi, wasm)
-    const gasLimit = getGasLimit(
-      api,
-      new BN(1_000_000_000),
-      new BN(5_000_000_000_000),
-    )
-    const constructorFn = code.tx[stringCamelCase(constructorMethod)]
-    const unsub = await constructorFn(
-      { gasLimit, ...options },
-      ...args,
-    ).signAndSend(account, ({ contract, status }: any) => {
-      if (status?.isInBlock) {
-        const hash = abi?.source.hash
-        const address = contract.address.toString()
-        console.log(`Contract '${abi?.contract.name}' deployed at ${address}`)
+  return new Promise<{ address: string; hash: string }>(
+    async (resolve, reject) => {
+      const code = new CodePromise(api, abi, wasm)
+      const gasLimit = getGasLimit(
+        api,
+        new BN(100_000_000_000),
+        new BN(5_000_000_000_000),
+      )
+      const constructorFn = code.tx[stringCamelCase(constructorMethod)]
+      const unsub = await constructorFn(
+        { gasLimit, ...options },
+        ...args,
+      ).signAndSend(account, ({ events, contract, status }: any) => {
+        if (status?.isInBlock) {
+          unsub?.()
 
-        unsub?.()
-        resolve({
-          address,
-          hash,
-        })
-      }
-    })
-  })
+          const extrinsicFailedEvent = events.find(
+            ({ event: { method } }: any) => method === 'ExtrinsicFailed',
+          ) as EventRecord
+          if (!!extrinsicFailedEvent || !contract?.address) {
+            console.error(
+              `Contract '${abi?.contract.name}' could not be deployed`,
+              extrinsicFailedEvent?.event?.data?.toHuman(),
+            )
+            return reject()
+          }
+
+          const hash = abi?.source.hash
+          const address = contract.address.toString()
+          console.log(`Contract '${abi?.contract.name}' deployed at ${address}`)
+
+          return resolve({
+            address,
+            hash,
+          })
+        }
+      })
+    },
+  )
 }
