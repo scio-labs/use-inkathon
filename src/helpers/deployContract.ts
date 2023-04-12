07ce560b2188719f9db4747c5a4ebde27af3410c
Dennis Zoma <dennis@scio.xyz>
2023-04-12 20:10:36 +0200

    feat: Return block number of deployment in `deployContract`

--- src/helpers/deployContract.ts
+++ src/helpers/deployContract.ts
@@ -1,7 +1,7 @@
 import { ApiPromise } from '@polkadot/api'
 import { CodePromise } from '@polkadot/api-contract'
 import { ContractOptions } from '@polkadot/api-contract/types'
-import { EventRecord } from '@polkadot/types/interfaces'
+import { EventRecord, SignedBlock } from '@polkadot/types/interfaces'
 import { IKeyringPair } from '@polkadot/types/types'
 import { stringCamelCase } from '@polkadot/util'
 import { getMaxGasLimit } from './getGasLimit'
@@ -18,39 +18,52 @@ export const deployContract = async (
   args = [] as unknown[],
   options = {} as ContractOptions,
 ) => {
-  return new Promise<{ address: string; hash: string }>(
-    async (resolve, reject) => {
-      const code = new CodePromise(api, abi, wasm)
-      const gasLimit = getMaxGasLimit(api)
-      const constructorFn = code.tx[stringCamelCase(constructorMethod)]
-      const unsub = await constructorFn(
-        { gasLimit, ...options },
-        ...args,
-      ).signAndSend(account, ({ events, contract, status }: any) => {
-        if (status?.isInBlock) {
-          unsub?.()
+  return new Promise<{
+    address: string
+    hash: string
+    block: SignedBlock
+    blockNumber: number
+  }>(async (resolve, reject) => {
+    const code = new CodePromise(api, abi, wasm)
+    const gasLimit = getMaxGasLimit(api)
+    const constructorFn = code.tx[stringCamelCase(constructorMethod)]
+    const unsub = await constructorFn(
+      { gasLimit, ...options },
+      ...args,
+    ).signAndSend(account, async ({ events, contract, status }: any) => {
+      if (status?.isInBlock) {
+        unsub?.()
 
-          const extrinsicFailedEvent = events.find(
-            ({ event: { method } }: any) => method === 'ExtrinsicFailed',
-          ) as EventRecord
-          if (!!extrinsicFailedEvent || !contract?.address) {
-            console.error(
-              `Contract '${abi?.contract.name}' could not be deployed`,
-              extrinsicFailedEvent?.event?.data?.toHuman(),
-            )
-            return reject()
-          }
-
-          const hash = abi?.source.hash
-          const address = contract.address.toString()
-          console.log(`Contract '${abi?.contract.name}' deployed at ${address}`)
-
-          return resolve({
-            address,
-            hash,
-          })
+        const extrinsicFailedEvent = events.find(
+          ({ event: { method } }: any) => method === 'ExtrinsicFailed',
+        ) as EventRecord
+        if (!!extrinsicFailedEvent || !contract?.address) {
+          console.error(
+            `Contract '${abi?.contract.name}' could not be deployed`,
+            extrinsicFailedEvent?.event?.data?.toHuman(),
+          )
+          return reject()
         }
-      })
-    },
-  )
+
+        const hash = abi?.source.hash
+        const address = contract.address.toString()
+
+        // Determine block number
+        const blockHash = status.asInBlock.toHex()
+        const block = await api.rpc.chain.getBlock(blockHash)
+        const blockNumber = block.block.header.number.toNumber()
+
+        console.log(
+          `Contract '${abi?.contract.name}' deployed under ${address} at block #${blockNumber}`,
+        )
+
+        return resolve({
+          address,
+          hash,
+          block,
+          blockNumber,
+        })
+      }
+    })
+  })
 }
