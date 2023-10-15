8156c6ad3862ca8fb17981556126bce88c11ffb4
Dennis Zoma <dennis@scio.xyz>
2023-10-15 13:28:13 +0200

    feat: Add `simple-git-hooks` & Replace `husky`

--- .simple-git-hooks.js
+++ .simple-git-hooks.js
@@ -1,7 +1,8 @@
-#!/usr/bin/env sh
-. "$(dirname -- "$0")/_/husky.sh"
-
+module.exports = {
+  'pre-commit': `
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
-npx lint-staged
+pnpm lint-staged
+  `,
+}
