ec062889cc66321cee69c0e9904ce9810587a303
ical10 <mochhusnir@gmail.com>
2023-10-20 15:11:41 +0700

    feat: Add support for all ink!-compatible chains

--- src/chains.ts
+++ src/chains.ts
@@ -74,6 +74,91 @@ export const shibuya: SubstrateChain = {
   faucetUrls: ['https://portal.astar.network/#/shibuya-testnet/assets'],
 }
 
+export const t0rn: SubstrateChain = {
+  network: 't0rn',
+  name: 'T0rn Testnet',
+  ss58Prefix: 42,
+  rpcUrls: ['wss://ws.t0rn.io'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://ws.t0rn.io',
+    )}/#/explorer`,
+  },
+  testnet: true,
+  faucetUrls: ['https://faucet.t0rn.io'],
+}
+
+export const bitCountryAlphaTestnet: SubstrateChain = {
+  network: 'bitcountry-alpha-testnet',
+  name: 'Bit.Country Alpha Testnet',
+  ss58Prefix: 268,
+  rpcUrls: ['wss://alphanet-rpc-gcp.bit.country'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://alphanet-rpc-gcp.bit.country',
+    )}#/explorer`,
+  },
+  testnet: true,
+  faucetUrls: ['https://testnet.bit.country/p/wallet/balance'],
+}
+
+export const agungTestnet: SubstrateChain = {
+  network: 'agung-testnet',
+  name: 'Agung Testnet (Peaq Network)',
+  ss58Prefix: 42,
+  rpcUrls: ['wss://wss.agung.peaq.network'],
+  explorerUrls: {
+    [SubstrateExplorer.Subscan]: `https://agung-testnet.subscan.io/`,
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://wss.agung.peaq.network',
+    )}/#/explorer`,
+  },
+  testnet: true,
+  faucetUrls: ['https://discord.com/channels/943486047625572392/963415793394143232'],
+}
+
+export const rococoAmplitudeTestnet: SubstrateChain = {
+  network: 'rococo-amplitude-testnet',
+  name: 'Amplitude Testnet (Foucoco)',
+  ss58Prefix: 57,
+  rpcUrls: ['wss://pencol-roc-00.pendulumchain.tech'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://pencol-roc-00.pendulumchain.tech',
+    )}/#/explorer`,
+  },
+  testnet: true,
+  faucetUrls: [],
+}
+
+export const phalaPOC5Testnet: SubstrateChain = {
+  network: 'phala-PoC-5-testnet',
+  name: 'Phala PoC-5 Testnet',
+  ss58Prefix: 30,
+  rpcUrls: ['wss://poc5.phala.network'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://poc5.phala.network/ws',
+    )}#/explorer`,
+  },
+  testnet: true,
+  faucetUrls: [],
+}
+
+export const phalaPOC6Testnet: SubstrateChain = {
+  network: 'phala-PoC-6-testnet',
+  name: 'Phala PoC-6 Testnet',
+  ss58Prefix: 30,
+  rpcUrls: ['wss://poc6.phala.network'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://poc6.phala.network/ws',
+    )}#/explorer`,
+  },
+  testnet: true,
+  faucetUrls: [],
+}
+
 /// Canary Networks (Kusama)
 
 export const shiden: SubstrateChain = {
@@ -83,6 +168,36 @@ export const shiden: SubstrateChain = {
   rpcUrls: ['wss://shiden-rpc.dwellir.com'],
   explorerUrls: {
     [SubstrateExplorer.Subscan]: `https://shiden.subscan.io`,
+  },
+}
+
+export const amplitude: SubstrateChain = {
+  network: 'amplitude',
+  name: 'Amplitude',
+  ss58Prefix: 57,
+  rpcUrls: ['wss://rpc-amplitude.pendulumchain.tech', 'wss://amplitude-rpc.dwellir.com'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://rpc-amplitude.pendulumchain.tech',
+    )}#/explorer`,
+  },
+}
+
+export const khala: SubstrateChain = {
+  network: 'khala',
+  name: 'Khala',
+  ss58Prefix: 30,
+  rpcUrls: [
+    'wss://khala-api.phala.network/ws',
+    'wss://khala.api.onfinality.io/public-ws',
+    'wss://khala-rpc.dwellir.com',
+    'wss://public-rpc.pinknode.io/khala',
+  ],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://khala-api.phala.network/ws',
+    )}#/explorer`,
+    [SubstrateExplorer.Subscan]: `https://khala.subscan.io`,
   },
 }
 
@@ -109,7 +224,31 @@ export const astar: SubstrateChain = {
   explorerUrls: {
     [SubstrateExplorer.Subscan]: `https://astar.subscan.io`,
   },
-  faucetUrls: [],
+}
+
+export const pendulum: SubstrateChain = {
+  network: 'pendulum',
+  name: 'Pendulum',
+  ss58Prefix: 56,
+  rpcUrls: ['wss://rpc-pendulum.prd.pendulumchain.tech', 'wss://pendulum-rpc.dwellir.com'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://rpc-pendulum.prd.pendulumchain.tech',
+    )}#/explorer`,
+  },
+}
+
+export const phala: SubstrateChain = {
+  network: 'phala',
+  name: 'Phala',
+  ss58Prefix: 30,
+  rpcUrls: ['wss://api.phala.network/ws', 'wss://phala.api.onfinality.io/public-ws'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=${encodeURIComponent(
+      'wss://api.phala.network/ws',
+    )}#/explorer`,
+    [SubstrateExplorer.Subscan]: `https://phala.subscan.io`,
+  },
 }
 
 /**
@@ -119,10 +258,20 @@ export const allSubstrateChains: SubstrateChain[] = [
   development,
   alephzeroTestnet,
   rococo,
+  t0rn,
+  bitCountryAlphaTestnet,
+  agungTestnet,
+  rococoAmplitudeTestnet,
+  phalaPOC5Testnet,
+  phalaPOC6Testnet,
   shibuya,
   shiden,
+  amplitude,
+  khala,
   alephzero,
   astar,
+  pendulum,
+  phala,
 ]
 
 /**
