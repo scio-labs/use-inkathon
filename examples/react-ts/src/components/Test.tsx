c91d47633b3247c805ea3fcfb1008351a498bef1
Aurora Poppyseed <aurora.makovac@gmail.com>
2024-01-08 17:46:12 +0100

    better format

--- examples/react-ts/src/components/Test.tsx
+++ examples/react-ts/src/components/Test.tsx
@@ -1,14 +1,31 @@
-import { BrokerConstantsType, getConstants } from '@poppyseed/lastic-sdk'
+import {
+  BrokerConstantsType,
+  getConstants
+} from '@poppyseed/lastic-sdk'
 
 export function Test() {
   const brokerConstants: BrokerConstantsType | null = getConstants()
-  console.log("broker const: :", brokerConstants)
-
+  
+  if (brokerConstants === null) {
+    return (
+      <div>
+        <h3>Broker Constants:</h3>
+        <pre>not connected</pre>
+        <h3>Broker Extrinsics:</h3>
+        <pre>not connected</pre>
+      </div>
+    )
+  }
 
   return (
     <div>
         <h3>Broker Constants:</h3>
-        <pre>{JSON.stringify(brokerConstants, null, 2)}</pre>
+        <p>PalletId: {brokerConstants.palletId}</p>
+        <p>Timeslice Period: {brokerConstants.timeslicePeriod}</p>
+        <p>Max Leased Cores: {brokerConstants.maxLeasedCores}</p>
+        <p>Max reserved Cores: {brokerConstants.maxReservedCores}</p>
+        <h3>Broker Extrinsics:</h3>
+
     </div>
   )
 }
