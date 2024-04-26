aeee0e6098660728fefa5a2391deb349d3b4d664
peetzweg/ <phil.czek@gmail.com>
2024-04-26 13:43:43 +0200

    adds smart contract capable Pop Network

--- src/chains.ts
+++ src/chains.ts
@@ -36,6 +36,18 @@ export const alephzeroTestnet: SubstrateChain = {
   faucetUrls: ['https://faucet.test.azero.dev'],
 }
 
+export const popNetwork: SubstrateChain = {
+  network: 'pop-network',
+  name: 'Pop Network',
+  ss58Prefix: 42,
+  rpcUrls: ['wss://rpc1.paseo.popnetwork.xyz', 'wss://rpc2.paseo.popnetwork.xyz'],
+  explorerUrls: {
+    [SubstrateExplorer.PolkadotJs]: `https://polkadot.js.org/apps/?rpc=wss%3A%2F%2Frpc1.paseo.popnetwork.xyz`,
+  },
+  testnet: true,
+  faucetUrls: ['https://faucet.polkadot.io/'],
+}
+
 export const contracts: SubstrateChain = {
   network: 'contracts',
   name: 'Contracts on Rococo',
